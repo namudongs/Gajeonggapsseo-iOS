3355bc520482125d2c6c30e6bc01e9bddab3dfbb
namudi <namudongs@gmail.com>
2024-06-17 22:50:10 +0900

    [Setting] #1 - Xcode 프로젝트 기본 설정

---
+++ Gajeonggapsseo-iOS/Gajeonggapsseo-iOS/Gajeonggapsseo_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gajeonggapsseo_iOSApp.swift
+//  Gajeonggapsseo-iOS
+//
+//  Created by namdghyun on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Gajeonggapsseo_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
