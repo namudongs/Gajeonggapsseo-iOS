3f8627efd4d74792f9dcd471600a7eed3adecf71
daminoworld <ekadls757@gmail.com>
2024-06-25 23:41:05 +0900

    [Chore] #19 - Add Color+ extension
    - hex코드로 Color를 지정할 수 있는 extension추가

--- Gajeonggapsseo-iOS/Extensions/Color+.swift
+++ Gajeonggapsseo-iOS/Extensions/Color+.swift
@@ -5,4 +5,33 @@
 //  Created by Damin on 6/25/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    
+    init(hex: String, alpha: Double = 1.0) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6:
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8:
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: alpha
+        )
+    }
+}
+
