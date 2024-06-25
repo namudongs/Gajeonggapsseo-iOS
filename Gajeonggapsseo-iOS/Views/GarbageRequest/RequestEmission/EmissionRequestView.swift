4ded16ca2ee276e6253712870a2fa334b28bb6ac
daminoworld <ekadls757@gmail.com>
2024-06-25 22:23:24 +0900

    [Add] #19 - Add EmissionRequestView file
    - Extensions 그룹을 추가해 Color+ 추가

---
+++ Gajeonggapsseo-iOS/Views/GarbageRequest/RequestEmission/EmissionRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  EmissionRequestView.swift
+//  Gajeonggapsseo-iOS
+//
+//  Created by Damin on 6/25/24.
+//
+
+import SwiftUI
+
+struct EmissionRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmissionRequestView()
+}
