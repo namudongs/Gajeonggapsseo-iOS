d6d92c34031156a518a352b554c17f8fc0a5dcb4
frankwon11 <frankwon11@gmail.com>
2024-06-30 02:35:15 +0900

    [Design] #26 - 버튼 레이블 디자인 추가

---
+++ Gajeonggapsseo-iOS/Views/GarbageRequest/Components/ButtonLabel.swift
@@ -0,0 +1,45 @@
+//
+//  ButtonLabel.swift
+//  Gajeonggapsseo-iOS
+//
+//  Created by sseungwonnn on 6/30/24.
+//
+
+import SwiftUI
+
+struct ButtonLabel: View {
+    var content: String
+    var isAgentRequst: Bool
+    var isDisabled: Bool
+    
+    var labelColor: Color {
+        if isDisabled {
+            Color(hex: "C4C4C4")
+        } else {
+            if isAgentRequst {
+                .blue
+            } else {
+                .requestAgent
+            }
+        }
+    }
+    var body: some View {
+        HStack {
+            Spacer()
+            Text("\(content)")
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundColor(.white)
+            Spacer()
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 55)
+                .frame(height: 68)
+                .foregroundColor(labelColor)
+        )
+    }
+}
+
+#Preview {
+    ButtonLabel(content: "배출 완료하기", isAgentRequst: false, isDisabled: false)
+}
