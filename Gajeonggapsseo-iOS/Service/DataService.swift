7d5b276cdaee9efca1c9be3532a3a0cdaaeb2cb4
namudi <namudongs@gmail.com>
2024-07-03 03:49:59 +0900

    [Feat] #42 - Reject으로 인한 수정

--- Gajeonggapsseo-iOS/Service/DataService.swift
+++ Gajeonggapsseo-iOS/Service/DataService.swift
@@ -13,25 +13,21 @@ class CleanHouseService: ApiService {
     typealias ResponseType = CleanHouseWelcome
     
     func fetchData(completion: @escaping (Result<CleanHouseWelcome, Error>) -> Void) {
-        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] else { return }
-        let baseUrl = "http://apis.data.go.kr/6510000/cleanHouseService/getCleanHouseList"
-        guard let url = URL(string: "\(baseUrl)?serviceKey=\(apiKey)&pageNo=1&numOfRows=1374") else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else { return }
-            do {
-                let decodedResponse = try JSONDecoder().decode(CleanHouseWelcome.self, from: data)
-                completion(.success(decodedResponse))
-            } catch let error {
-                print("Jeju CleanHouse Decoded Error")
-                completion(.failure(error))
-            }
+        guard let url = Bundle.main.url(forResource: "JejuCleanHouse", withExtension: "json") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
+            return
         }
-        task.resume()
+        
+        do {
+            let data = try Data(contentsOf: url)
+            
+            let decoder = JSONDecoder()
+            let decodedResponse = try decoder.decode(CleanHouseWelcome.self, from: data)
+            completion(.success(decodedResponse))
+        } catch {
+            print("Jeju CleanHouse Decoded Error: \(error)")
+            completion(.failure(error))
+        }
     }
 }
 
@@ -39,25 +35,19 @@ class RecycleCenterService: ApiService {
     typealias ResponseType = RecycleCenterWelcome
     
     func fetchData(completion: @escaping (Result<RecycleCenterWelcome, Error>) -> Void) {
-        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] else { return }
-        let baseUrl = "http://apis.data.go.kr/6510000/recycleCenterService/getRecycleCenterList"
-        guard let url = URL(string: "\(baseUrl)?serviceKey=\(apiKey)&pageNo=1&numOfRows=1374") else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else { return }
-            do {
-                let decodedResponse = try JSONDecoder().decode(RecycleCenterWelcome.self, from: data)
-                completion(.success(decodedResponse))
-            } catch let error {
-                print("Jeju RecycleCenter Decoded Error")
-                completion(.failure(error))
-            }
+        guard let url = Bundle.main.url(forResource: "JejuRecycleCenter", withExtension: "json") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
+            return
         }
-        task.resume()
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decodedResponse = try JSONDecoder().decode(RecycleCenterWelcome.self, from: data)
+            completion(.success(decodedResponse))
+        } catch {
+            print("Jeju RecycleCenter Decoded Error")
+            completion(.failure(error))
+        }
     }
 }
 
@@ -85,25 +75,19 @@ class SeogwipoRecycleCenterService: ApiService {
     typealias ResponseType = SeogwipoRecycleCenterApiResponse
     
     func fetchData(completion: @escaping (Result<SeogwipoRecycleCenterApiResponse, Error>) -> Void) {
-        guard let apiKey = Bundle.main.infoDictionary?["API_KEY"] else { return }
-        let baseUrl = "https://api.odcloud.kr/api/15108248/v1/uddi:55cc390d-ad9c-4398-b793-f1d851a11440"
-        guard let url = URL(string: "\(baseUrl)?page=1&perPage=55&serviceKey=\(apiKey)") else { return }
-        
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else { return }
-            do {
-                let decodedResponse = try JSONDecoder().decode(SeogwipoRecycleCenterApiResponse.self, from: data)
-                completion(.success(decodedResponse))
-            } catch let error {
-                print("Seogwipo RecycleCenter Decoded Error")
-                completion(.failure(error))
-            }
+        guard let url = Bundle.main.url(forResource: "SeogwippoRecycleCenter", withExtension: "json") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: -1, userInfo: nil)))
+            return
         }
-        task.resume()
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decodedResponse = try JSONDecoder().decode(SeogwipoRecycleCenterApiResponse.self, from: data)
+            completion(.success(decodedResponse))
+        } catch {
+            print("Seogwipo RecycleCenter Decoded Error")
+            completion(.failure(error))
+        }
     }
 }
 
